79951f8e0129be08206291f20d5313470892e7e0
AdrianDali <adrianhernandez@aragon.unam.mx>
2021-09-01 10:06:39 -0500

    primer commit

---
+++ Septiembre_1/telefono.py
@@ -0,0 +1,14 @@
+
+def numeroTelefono(numero):
+    print(numero.replace(' ', ''))
+    print (numero[0:3])
+
+
+
+def run():
+    numeroTelefono("+52 55481216 56")
+
+    
+
+if __name__ == "__main__":    
+    run()
