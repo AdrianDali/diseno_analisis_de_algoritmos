8230f9f6ea06e23f430f96b43316b305a9a313c5
AdrianDali <hadriandali@gmail.com>
2021-09-16 16:04:30 -0500

    Tarea 3

---
+++ Tarea3/combinaciones.py
@@ -0,0 +1,15 @@
+#Adrian Dali Hernandez Rueda
+def combinacion():
+    cadena = [1,2,3]
+    for item in cadena:
+        for apuntador in cadena:
+            print( item,' , ',apuntador,'\n' )
+            
+    
+
+def run():
+    combinacion()
+
+
+if __name__ == "__main__":
+    run()
