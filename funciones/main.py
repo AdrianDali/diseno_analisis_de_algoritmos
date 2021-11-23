5df8da7056423295faeefa1c4a09c6b17b77f63c
AdrianDali <hadriandali@gmail.com>
2021-11-22 19:44:25 -0600

    Avancez

--- funciones/main.py
+++ funciones/main.py
@@ -1,15 +1,37 @@
 from matplotlib import pyplot
-# Función cuadrática.
+
+e = 2.71
+
+# FUNCIONES CUADRATICAS.
 def f1(x):
-    return 2*(x**2) + 5*x - 2
-# Función lineal.
+    return 2*(x**2) 
 def f2(x):
-    return 4*x + 1
+    return 5*(x**2) 
+def f3(x):
+    return -15*(x**2)
+
+#FUNCIONES CUBICAS 
+def f4(x):
+    return (x**3)
+def f5(x):
+    return 4*(x**3)
+
+def f6(x):
+    return 0 if x == 0.0 else 2/x
+
+def f7(x):
+    return e**x 
 # Valores del eje X que toma el gráfico.
 x = range(-10, 15)
 # Graficar ambas funciones.
-pyplot.plot(x, [f1(i) for i in x])
-pyplot.plot(x, [f2(i) for i in x])
+#pyplot.plot(x, [f1(i) for i in x],label="f1")
+#pyplot.plot(x, [f2(i) for i in x],label = "f2")
+#pyplot.plot(x, [f3(i) for i in x],label = "f3")
+#pyplot.plot(x, [f4(i) for i in x],label = "f4")
+#pyplot.plot(x, [f5(i) for i in x],label = "f5")
+#pyplot.plot(x, [f6(i) for i in x],label = "f6")
+pyplot.plot(x, [f7(i) for i in x],label = "f7")
+
 # Establecer el color de los ejes.
 pyplot.axhline(0, color="black")
 pyplot.axvline(0, color="black")
@@ -18,5 +40,6 @@ pyplot.xlim(-10, 10)
 pyplot.ylim(-10, 10)
 # Guardar gráfico como imágen PNG.
 pyplot.savefig("output.png")
+pyplot.legend()
 # Mostrarlo.
 pyplot.show()
